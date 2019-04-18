8449f82f58dedaa1f5d484332e7897d7c909914d
Gleb Bondarenko <gebond77@gmail.com>
2019-04-18 22:04:49 +0200

    Add

--- barracuda-be/app.js
+++ barracuda-be/app.js
@@ -1,1 +1,15 @@
+// Include http module.
+var http = require('http');
 
+// Create http server.
+var httpServer = http.createServer(function (req, resp) {
+
+    resp.writeHeader(200);
+
+    resp.end('This is a node js created http web server. ');
+});
+
+// Http server listen on port 5000.
+httpServer.listen(5000);
+
+console.log("Http web server listening on port 5000. Access it with url http://localhost:5000.");
